f4bde281761d9d75ca2d76cfdf5db1038b53afd6
Brandon Ha <hsy0602@outlook.com>
2021-01-03 16:18:56 +0900

    #4 - Objects & Arrays

--- finance-logger/sandbox.ts
+++ finance-logger/sandbox.ts
@@ -9,3 +9,21 @@ const circ = (diameter: number) => {
 };
 
 console.log(circ(2));
+
+// #4 - Objects & Arrays
+// arrary
+let names = ["roger", "oden", "layigh"];
+
+names[0] = "2";
+
+let mixed = [2, 3, "gold", "luffy"];
+names[0] = "hi";
+mixed.push(5);
+
+// object
+let ninja = { name: "kiado", belt: "black", age: 60 };
+
+// ninja.age = '40';
+
+// can add another value by creating the exact same match
+ninja = { name: "bigmom", belt: "white", age: 50 };
