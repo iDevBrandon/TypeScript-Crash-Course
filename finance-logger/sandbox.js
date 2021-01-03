41c5508a78bb8e0b4da1c5e64b8c17b22d58dbd8
Brandon Ha <hsy0602@outlook.com>
2021-01-03 16:36:38 +0900

    TypeScript Tutorial #5 - Explicit Types

--- finance-logger/sandbox.js
+++ finance-logger/sandbox.js
@@ -1,8 +1,40 @@
-var character = "mario";
-var age = 30;
-var isBlackBelt = false;
-character = "luigi";
-var circ = function (diameter) {
-    return diameter * Math.PI;
-};
-console.log(circ(2));
+// let character = "mario";
+// let age: number = 30;
+// let isBlackBelt = false;
+// character = "luigi";
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+// console.log(circ(2));
+// // #4 - Objects & Arrays
+// // arrary
+// let names = ["roger", "oden", "layigh"];
+// names[0] = "2";
+// let mixed = [2, 3, "gold", "luffy"];
+// names[0] = "hi";
+// mixed.push(5);
+// // object
+// let ninja = { name: "kiado", belt: "black", age: 60 };
+// // ninja.age = '40';
+// // can add another value by creating the exact same match
+// ninja = { name: "bigmom", belt: "white", age: 50 };
+// TypeScript Tutorial #5 - Explicit Types
+// explicit types
+var character;
+var age;
+var isLoggedIn;
+age = 30;
+isLoggedIn = true;
+// arrays
+var ninja = [];
+ninja.push("shan");
+// union types
+var mixed = [];
+mixed.push("shanks");
+mixed.push(20);
+mixed.push(true);
+console.log(mixed);
+// objects
+var ninjaOne;
+ninjaOne = { name: "ninja", age: 30 };
+var ninjaTwo;
--- finance-logger/sandbox.ts
+++ finance-logger/sandbox.ts
@@ -1,29 +1,60 @@
-let character = "mario";
-let age: number = 30;
-let isBlackBelt = false;
+// let character = "mario";
+// let age: number = 30;
+// let isBlackBelt = false;
 
-character = "luigi";
+// character = "luigi";
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+// const circ = (diameter: number) => {
+//   return diameter * Math.PI;
+// };
+
+// console.log(circ(2));
+
+// // #4 - Objects & Arrays
+// // arrary
+// let names = ["roger", "oden", "layigh"];
+
+// names[0] = "2";
+
+// let mixed = [2, 3, "gold", "luffy"];
+// names[0] = "hi";
+// mixed.push(5);
+
+// // object
+// let ninja = { name: "kiado", belt: "black", age: 60 };
+
+// // ninja.age = '40';
+
+// // can add another value by creating the exact same match
+// ninja = { name: "bigmom", belt: "white", age: 50 };
+
+// TypeScript Tutorial #5 - Explicit Types
+// explicit types
+let character: string;
+let age: number;
+let isLoggedIn: boolean;
+
+age = 30;
+isLoggedIn = true;
+
+// arrays
+let ninja: string[] = [];
+
+ninja.push("shan");
+
+// union types
+let mixed: (string | number | boolean)[] = [];
+mixed.push("shanks");
+mixed.push(20);
+mixed.push(true);
+console.log(mixed);
+
+// objects
+let ninjaOne: object;
+ninjaOne = { name: "ninja", age: 30 };
+
+let ninjaTwo: {
+  name: string;
+  email: string;
+  age: number;
 };
-
-console.log(circ(2));
-
-// #4 - Objects & Arrays
-// arrary
-let names = ["roger", "oden", "layigh"];
-
-names[0] = "2";
-
-let mixed = [2, 3, "gold", "luffy"];
-names[0] = "hi";
-mixed.push(5);
-
-// object
-let ninja = { name: "kiado", belt: "black", age: 60 };
-
-// ninja.age = '40';
-
-// can add another value by creating the exact same match
-ninja = { name: "bigmom", belt: "white", age: 50 };
